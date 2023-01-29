a6752ddad86f6d254505ae5464574a10e8ddeb8a
Ruchi Ray <ruchiray18@gmail.com>
2023-01-29 16:00:51 +0530

    set bg properly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import bg from "./bg.png";
 import styled, { createGlobalStyle } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 const Wrapper = styled.section`
-  background: url(${bg}) no-repeat center center/cover;
+  background: url(${bg}) no-repeat center 100%/cover;
   height: 99.5vh;
   width: 100%;
   min-height: 600px;
